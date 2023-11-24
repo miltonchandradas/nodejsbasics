9ac2228db7abbfdccc21551901bf45c8a5900ae7
Milton Chandradas <milton.chandradas@sap.com>
2023-11-24 05:48:03 +0530

    initial commit...

--- server.js
+++ server.js
@@ -1,25 +1,47 @@
-// Object definition
-let person = {
-  name: "John Doe",
-  age: 25,
-  city: "Example City",
+// Array definition
+let numbers = [1, 2, 3, 4, 5];
+
+// Function to display array elements
+const displayArray = (arr) => {
+  console.log("Array:", arr.join(", "));
 };
 
-// Function to display information about the person using template literals
-const displayPersonInfo = (person) => {
-  console.log(`Name: ${person.name}`);
-  console.log(`Age: ${person.age}`);
-  console.log(`City: ${person.city}`);
-};
+// Display original array
+console.log("Original array:");
+displayArray(numbers);
 
-// Display original information about the person
-console.log("Original information:");
-displayPersonInfo(person);
+// Add an element to the end of the array
+numbers.push(6);
 
-// Manipulate the object: Add a new property and update an existing one
-person.email = "john.doe@example.com";
-person.age = 26;
+// Display array after adding an element
+console.log("\nArray after adding 6 to the end:");
+displayArray(numbers);
 
-// Display updated information about the person
-console.log("\nUpdated information:");
-displayPersonInfo(person);
+// Remove the last element from the array
+numbers.pop();
+
+// Display array after removing the last element
+console.log("\nArray after removing the last element:");
+displayArray(numbers);
+
+// Add an element to the beginning of the array
+numbers.unshift(0);
+
+// Display array after adding 0 to the beginning
+console.log("\nArray after adding 0 to the beginning:");
+displayArray(numbers);
+
+// Remove the first element from the array
+numbers.shift();
+
+// Display array after removing the first element
+console.log("\nArray after removing the first element:");
+displayArray(numbers);
+
+// Find the index of a specific element
+const indexOfThree = numbers.indexOf(3);
+console.log("\nThe index of 3 in the array:", indexOfThree);
+
+// Slice the array to create a new array with specific elements
+const slicedArray = numbers.slice(1, 4);
+console.log("\nSliced array (from index 1 to 4):", slicedArray);
