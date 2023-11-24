133289765dfd8be0af451b32117b8fbb790707ba
Milton Chandradas <milton.chandradas@sap.com>
2023-11-24 05:43:54 +0530

    initial commit...

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
-// function
-const isEven = (number) => {
-  // Use the modulo operator (%) to check if the number is even
-  if (number % 2 === 0) {
-    return true; // The number is even
-  } else {
-    return false; // The number is odd
-  }
+// Object definition
+let person = {
+  name: "John Doe",
+  age: 25,
+  city: "Example City",
 };
 
-// Example usage
-var num1 = 10;
-var num2 = 7;
+// Function to display information about the person using template literals
+const displayPersonInfo = (person) => {
+  console.log(`Name: ${person.name}`);
+  console.log(`Age: ${person.age}`);
+  console.log(`City: ${person.city}`);
+};
 
-console.log(`${num1} is even: ${isEven(num1)}`);
-console.log(`${num2} is even: ${isEven(num2)}`);
+// Display original information about the person
+console.log("Original information:");
+displayPersonInfo(person);
+
+// Manipulate the object: Add a new property and update an existing one
+person.email = "john.doe@example.com";
+person.age = 26;
+
+// Display updated information about the person
+console.log("\nUpdated information:");
+displayPersonInfo(person);
