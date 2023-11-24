02d4b8c0701d9f630b8dd1f9282764e2d70f4d10
Milton Chandradas <milton.chandradas@sap.com>
2023-11-25 03:02:36 +0530

    initial commit...

--- server.js
+++ server.js
@@ -1,47 +1,8 @@
-// Array definition
-let numbers = [1, 2, 3, 4, 5];
+// Array of numbers
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// Function to display array elements
-const displayArray = (arr) => {
-  console.log("Array:", arr.join(", "));
-};
+// Using filter to get even numbers
+const evenNumbers = numbers.filter((number) => number % 2 === 0);
 
-// Display original array
-console.log("Original array:");
-displayArray(numbers);
-
-// Add an element to the end of the array
-numbers.push(6);
-
-// Display array after adding an element
-console.log("\nArray after adding 6 to the end:");
-displayArray(numbers);
-
-// Remove the last element from the array
-numbers.pop();
-
-// Display array after removing the last element
-console.log("\nArray after removing the last element:");
-displayArray(numbers);
-
-// Add an element to the beginning of the array
-numbers.unshift(0);
-
-// Display array after adding 0 to the beginning
-console.log("\nArray after adding 0 to the beginning:");
-displayArray(numbers);
-
-// Remove the first element from the array
-numbers.shift();
-
-// Display array after removing the first element
-console.log("\nArray after removing the first element:");
-displayArray(numbers);
-
-// Find the index of a specific element
-const indexOfThree = numbers.indexOf(3);
-console.log("\nThe index of 3 in the array:", indexOfThree);
-
-// Slice the array to create a new array with specific elements
-const slicedArray = numbers.slice(1, 4);
-console.log("\nSliced array (from index 1 to 4):", slicedArray);
+console.log(evenNumbers);
+// Output: [2, 4, 6, 8, 10]
