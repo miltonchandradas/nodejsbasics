7f526438b2c75cccb07b6c1dcee15cce96e27c4e
Milton Chandradas <milton.chandradas@sap.com>
2023-11-25 03:20:07 +0530

    initial commit...

--- server.js
+++ server.js
@@ -3,17 +3,16 @@ const people = [
   { name: 'Alice', age: 25 },
   { name: 'Bob', age: 17 },
   { name: 'Charlie', age: 30 },
-  { name: 'David', age: 15 },
+  { name: 'David', age: 65 },
   { name: 'Eva', age: 40 },
 ];
 
-// Using forEach to log the names to the console
-people.forEach((person) => {
-  console.log(person.name);
-});
-// Output:
-// Alice
-// Bob
-// Charlie
-// David
-// Eva
+// Using some to check if there are any senior citizens
+const hasSeniorCitizen = people.some((person) => person.age >= 65);
+
+if (hasSeniorCitizen) {
+  console.log('There is at least one senior citizen.');
+} else {
+  console.log('There are no senior citizens.');
+}
+// Output: There is at least one senior citizen.
