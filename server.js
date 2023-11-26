ebceb737aa0f6525328e0d435446ab41a2e8c07b
Milton Chandradas <milton.chandradas@sap.com>
2023-11-26 22:34:19 +0530

    initial commit...

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
 const axios = require("axios");
 
-// Function to make a simple HTTP PUT request using Axios
-const updatePost = async () => {
-  const postIdToUpdate = 1; // Replace with the ID of the post you want to update
+// Function to make a simple HTTP DELETE request using Axios
+const deletePost = async () => {
+  const postIdToDelete = 1; // Replace with the ID of the post you want to delete
 
   try {
-    const response = await axios.put(`https://jsonplaceholder.typicode.com/posts/${postIdToUpdate}`, {
-      title: 'Updated Post',
-      body: 'This is the updated content of the post.',
-      userId: 1, // Assuming user ID 1 for the example
-    });
-
-    console.log('Post updated:', response.data);
+    const response = await axios.delete(
+      `https://jsonplaceholder.typicode.com/posts/${postIdToDelete}`
+    );
+    console.log("Post deleted:", response.data);
   } catch (error) {
-    console.error('Error updating post:', error.message);
+    console.error("Error deleting post:", error.message);
   }
 };
 
-// Call the updatePost function
-updatePost();
+// Call the deletePost function
+deletePost();
