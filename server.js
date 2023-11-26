5586c6c9a6a30231c222dd9d576f430b8906c153
Milton Chandradas <milton.chandradas@sap.com>
2023-11-26 22:32:35 +0530

    initial commit...

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 const axios = require("axios");
 
-// Function to make a simple HTTP POST request using Axios
-const postData = async () => {
-  try {
-    const response = await axios.post(
-      "https://jsonplaceholder.typicode.com/posts",
-      {
-        title: "New Post",
-        body: "This is the content of the new post.",
-        userId: 1, // Assuming user ID 1 for the example
-      }
-    );
+// Function to make a simple HTTP PUT request using Axios
+const updatePost = async () => {
+  const postIdToUpdate = 1; // Replace with the ID of the post you want to update
 
-    console.log("Post created:", response.data);
+  try {
+    const response = await axios.put(`https://jsonplaceholder.typicode.com/posts/${postIdToUpdate}`, {
+      title: 'Updated Post',
+      body: 'This is the updated content of the post.',
+      userId: 1, // Assuming user ID 1 for the example
+    });
+
+    console.log('Post updated:', response.data);
   } catch (error) {
-    console.error("Error creating post:", error.message);
+    console.error('Error updating post:', error.message);
   }
 };
 
-// Call the postData function
-postData();
+// Call the updatePost function
+updatePost();
